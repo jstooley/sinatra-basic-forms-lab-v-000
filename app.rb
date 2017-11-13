17d4437e8ae38210c5e1a531b214c921ae97a968
Justin Tooley <jstooley823@gmail.com>
2017-11-13 03:39:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,9 +11,7 @@ class App < Sinatra::Base
   end
 
   post '/display' do
-    @puppy = params[:age]
-     params[:name]
-    params[:breed]
+    @puppy = (params[:name],params[:breed],params[:age])
     erb :display_puppy
   end
 
